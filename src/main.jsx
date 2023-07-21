ec6245a7b1141d3742f789bd86f7cfbeaa5c6a75
vaibahv-nayak18 <vaibhavnayak018@gmail.com>
2023-07-21 12:07:25 +0530

    fixed importing error in Main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
-import App from "./app";
+import App from "./App";
 import store from "./app/store";
 import { Provider } from "react-redux";
 
